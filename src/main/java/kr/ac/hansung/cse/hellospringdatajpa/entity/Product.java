c93c95aaafcff892460256785ed3db737a456c5d
kimgazii <gy0320ya@naver.com>
2025-06-08 14:24:44 +0900

    feat: 상품 등록/수정 시 유효성 검사

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/Product.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/Product.java
@@ -1,6 +1,9 @@
 package kr.ac.hansung.cse.hellospringdatajpa.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,9 +20,17 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "상품 이름은 필수입니다.")
     private String name;
+
+    @NotBlank(message = "브랜드는 필수입니다.")
     private String brand;
+
+    @NotBlank(message = "제조국가는 필수입니다.")
     private String madeIn;
+
+    @NotNull(message = "가격을 입력해주세요.")
+    @Min(value = 0, message = "가격은 0 이상이어야 합니다.")
     private double price;
 
     public Product(String name, String brand, String madeIn, double price) {
