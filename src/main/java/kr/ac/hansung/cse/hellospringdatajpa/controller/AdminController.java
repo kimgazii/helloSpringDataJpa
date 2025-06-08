978691d5360b84ad8525d698de4b2779061633d6
kimgazii <gy0320ya@naver.com>
2025-06-08 14:38:58 +0900

    feat: 관리자 페이지 구현

---
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/AdminController.java
@@ -0,0 +1,27 @@
+package kr.ac.hansung.cse.hellospringdatajpa.controller;
+
+import kr.ac.hansung.cse.hellospringdatajpa.entity.MyUser;
+import kr.ac.hansung.cse.hellospringdatajpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public String showAllUsers(Model model) {
+        List<MyUser> userList = userRepository.findAll();
+        model.addAttribute("userList", userList);
+        return "user_list";
+    }
+}
+
